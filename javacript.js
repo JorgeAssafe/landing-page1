c1b35a5314fc7e494e3327ce57856bfa79591706
JorgeAssafe <jorgeassafe22@gmail.com>
2024-01-25 11:55:58 +0100

    correção da lógica e alteração da cor do menu

--- javacript.js
+++ javacript.js
@@ -1,11 +1,10 @@
 function toggleMode () {
     const html = document.documentElement
-    html.classList.toggle('light')
-
+    
     if (html.classList.contains('light')){
-        html.classList.add('light')
+        html.classList.remove('light')
     }
     else {
-        html.classList.remove('light')
+        html.classList.add('light')
     }
 }
